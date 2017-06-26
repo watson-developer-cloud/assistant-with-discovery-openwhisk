51895f8e47d62f3e0b5bb2c5a42fdeac73dbea63
Christopher Sanchez <cvs42@cornell.edu>
2017-06-26 16:00:32 -0400

    Deleted copies of Watson components

--- client/public/components/Sizes.js
+++
@@ -1,3 +0,0 @@
-'use strict';Object.defineProperty(exports,"__esModule",{value:true});// Container sizes
-exports.default=['xx-small','x-small','small','medium','large','x-large','xx-large'];module.exports=exports['default'];
-//# sourceMappingURL=Sizes.js.map
