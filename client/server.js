9b0cb281a841358170fa91442a91e9bb41375bb3
Christopher Sanchez <cvs42@cornell.edu>
2017-06-27 14:38:00 -0400

    Move Folders out of 'Client'

--- client/server.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-
-const app = express();
-
-app.use(express.static('./node_modules/watson-react-components/dist/'));
-
-app.get("/", function(req, res) {
-    res.send("Helo")
-})
