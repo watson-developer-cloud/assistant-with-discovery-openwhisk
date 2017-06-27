9ed3c65f7516f5fa0f42cde9a687caa28f422faa
Christopher Sanchez <cvs42@cornell.edu>
2017-06-27 14:17:52 -0400

    wip

--- client/server.js
+++ client/server.js
@@ -1,3 +1,9 @@
 const express = require('express');
 
-const app = express.app();
+const app = express();
+
+app.use(express.static('./node_modules/watson-react-components/dist/'));
+
+app.get("/", function(req, res) {
+    res.send("Helo")
+})
