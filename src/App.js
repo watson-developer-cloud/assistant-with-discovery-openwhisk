b5494bae6a298dac239edc0045e10f1fd2cc2a76
Christopher Sanchez <cvs42@cornell.edu>
2017-07-12 15:26:35 -0400

    wip

--- src/App.js
+++ src/App.js
@@ -1,17 +1,91 @@
 import React, { Component } from 'react';
 import './App.css';
-import { Footer } from 'watson-react-components';
-import Conversation from './Conversation.js'
+import Conversation from './Conversation.js';
+import env from './env.js'
 
 class App extends Component {
-  render() {
-    return (
-      <div id="App">
-        <h1 id="header">Conversation with Discovery - Using OpenWhisk</h1>
-        <Conversation />
-      </div>
-    );
-  }
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            context: {},
+        // A Message Object consists of a message[, intent, date, isUser]
+            messageObjectList: []
+        };
+
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.callWatson("hello");
+    }
+
+    callWatson(message) {
+        const watsonApiUrl = env.API_URL;
+        const watsonApiKey = env.API_KEY;
+        const requestJson = JSON.stringify({
+            input: {
+                text: message
+            },
+            context: this.state.context
+        });
+        return fetch(watsonApiUrl,
+            {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Accept': 'application/json',
+                    'X-IBM-CLIENT-ID': watsonApiKey
+                },
+                body: requestJson
+            }
+        ).then((response) => response.json())
+        .then((responseJson) => {
+            responseJson.date = new Date();
+            this.handleResponse(responseJson);
+        });
+    }
+
+    handleResponse(responseJson) {
+        const outputMessage = responseJson.output.text.filter(text => text).join('\n');
+        const outputIntent = responseJson.intents[0] ? responseJson.intents[0]["intent"] : "";
+        const outputDate = responseJson.date;
+        const outputContext = responseJson.context;
+        this.setState({
+            context: outputContext
+        });
+        const msgObj = {
+            isUser: false,
+            intent: outputIntent,
+            content: outputMessage,
+            date: outputDate
+        };
+        this.addMessage(msgObj);
+    }
+
+    addMessage(msgObj) {
+        this.setState({
+            messageObjectList: [ ...this.state.messageObjectList , msgObj]
+        });
+    }
+
+    handleSubmit(e) {
+        const inputMessage = e.target.value;
+        const inputDate = new Date();
+        const msgObj = {
+            isUser: true,
+            intent: false,
+            content: inputMessage,
+            date: inputDate
+        };
+        this.addMessage(msgObj);
+        e.target.value = "";
+        this.callWatson(inputMessage);
+    }
+
+    render() {
+        return(
+            
+            <Conversation submitHandler={this.handleSubmit} messageObjectList={this.state.messageObjectList} />
+        );
+    }
 }
 
 export default App;
--- src/Conversation.js
+++ src/Conversation.js
@@ -1,121 +1,28 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './Conversation.css';
 import { InputWithButton } from 'watson-react-components';
-import Message from './Message.js';
-import DiscoveryResult from './DiscoveryResult.js';
-import JsonWindow from './JsonWindow.js';
-import env from './env.js';
 
-class Conversation extends Component {
-    constructor(props) {
-        super(props);
-        this.handleChange = this.handleChange.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
-        this.sendToWatson = this.sendToWatson.bind(this);
-        this.handleWatsonOutput = this.handleWatsonOutput.bind(this);
-        this.addMessageToList = this.addMessageToList.bind(this);
-        this.state = { messageList: [],
-                        text: "",
-                        context: {},
-        };
-        this.sendToWatson("");
-    }
+function Conversation(props) {
 
-    handleChange(e) {
-        this.setState({ text: e.target.value });
-    }
-
-    handleSubmit(e) {
-        const message = e.target.value;
-        this.addMessageToList(message, 'user');
-        this.setState({ text: ""});
-        e.target.value = "";
-        this.sendToWatson(message);
-    }
-
-    sendToWatson(message) {
-        const reqJson = JSON.stringify({
-            input: {
-                text: message
-            },
-            context: this.state.context
-        });
-        this.setState({ reqJson: reqJson });
-        return fetch(env.API_URL,
-            {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                    'Accept': 'application/json',
-                    'X-IBM-CLIENT-ID': env.API_KEY
-                },
-                body: reqJson
-            }
-        ).then((response) => response.json())
-        .then((responseJson) => {
-            this.handleWatsonOutput(responseJson);
-        })
-    }
-
-    handleWatsonOutput(json) {
-        this.setState({
-            context: json.context,
-            resJson: JSON.stringify(json)
-        })
-        if (json.output.hasOwnProperty("discoveryResults")) {
-            const resultArr = json.output.discoveryResults;
-            resultArr.map(function(result) {
-                this.addDiscoveryToList(result);
-            }.bind(this));
-        } else {
-            const text = json.output.text.filter(text => text).join('\n');
-            this.addMessageToList(text, 'watson');
-        }
-        
-        console.log(this.state.context);
-    }
-
-    addMessageToList(message, source) {
-        if (message !== "") {
-            this.setState({
-                messageList: [ ...this.state.messageList, {message: message, source: source}]
-            });
-        }
-    }
-
-    addDiscoveryToList(discoveryResult) {
-        this.setState({
-            messageList: [ ...this.state.messageList, {result: discoveryResult, source: 'discovery'}]
-        });
-    }
-
-    render() {
-        const messages = this.state.messageList.map(function(msg, index) {
-            if (msg.source === 'user') {
-                return <Message key={index} isUser={true} text={msg.message} />;
-            } else if (msg.source === 'watson') {
-                return <Message key={index} isUser={false} text={msg.message} />;
-            } else {
-                return <DiscoveryResult key={index} result={msg.result} />;
-            }
-        })
-        return (
-            <div id="conversation-container">
-                <JsonWindow reqJson={this.state.reqJson} resJson={this.state.resJson} />
-                <div id="message-container">
-                    {messages}
-                </div>
-                <div id="input-container">
-                    <InputWithButton
-                        placeholder={"Say something to Watson"}
-                        onInput={this.handleChange}
-                        onSubmit={this.handleSubmit}
-                    />
-                </div>
+    function makeMessage(msgObj, index) {
+        const formattedTime = msgObj.date.toLocaleTimeString();
+        return(
+            <div key={index} className={msgObj.isUser ? "message message--from-user" : "message message--from-watson"}>
+                {msgObj.intent ? <div className="message__intent">{msgObj.intent}</div> : false}
+                <div className="message__content">{msgObj.content}</div>
+                {msgObj.date ? <div className="message__date">{formattedTime}</div> : false}
             </div>
-        )
+        );
     }
-}
 
+    return(
+        <div className="conversation">
+            <div className="conversation__messages">
+                {props.messageObjectList.map(makeMessage)}
+            </div>
+            <InputWithButton className="conversation__input" onSubmit={props.submitHandler} placeholder="Say something to Watson."/>
+        </div>
+    );
+}
 
 export default Conversation;
