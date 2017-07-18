34d37f4e0d5326c0cb13275ea636a8a479feaeec
Christopher Sanchez <cvs42@cornell.edu>
2017-07-18 09:04:22 -0400

    Removed line

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
 import Conversation from './Conversation.js';
-//import env from './env.js'
 
 class App extends Component {
     constructor(props) {
