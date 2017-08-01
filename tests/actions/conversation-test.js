eea13f93504d86bc0989a3432e9a7c7109ef8f83
Christopher Sanchez <cvs42@cornell.edu>
2017-08-01 14:10:33 -0400

    :art: Linted tests

--- tests/actions/conversation-test.js
+++ tests/actions/conversation-test.js
@@ -6,40 +6,40 @@ const workspace_id = 'fake-id';
 
 describe('[action] Conversation', function() {
     
-    beforeEach(function() {
-        nock('https://gateway.watsonplatform.net:443')
-            .post(`/conversation/api/v1/workspaces/${workspace_id}/message`)
-            .query({
-                'version': '2017-05-26'
-            })
-            .reply(200, {
-                'fake-key': 'fake-value'
-            });
-    });
+  beforeEach(function() {
+    nock('https://gateway.watsonplatform.net:443')
+      .post(`/conversation/api/v1/workspaces/${workspace_id}/message`)
+      .query({
+        'version': '2017-05-26'
+      })
+      .reply(200, {
+        'fake-key': 'fake-value'
+      });
+  });
 
-    it('should throw error if credentials are missing', function() {
-        const params = {
-            input: 'fake input',
-            context: {},
-            workspace_id: workspace_id
-        };
-        return action.main(params).then(function() {
-            assert.fail('Missing credentials error was not found');
-        }).catch(function(error) {
-            assert(error.message === 'params.username cannot be null');
-        });
+  it('should throw error if credentials are missing', function() {
+    const params = {
+      input: 'fake input',
+      context: {},
+      workspace_id: workspace_id
+    };
+    return action.main(params).then(function() {
+      assert.fail('Missing credentials error was not found');
+    }).catch(function(error) {
+      assert(error.message === 'params.username cannot be null');
     });
+  });
 
-    it('should call Conversation when the parameters are right', function() {
-        const params = {
-            input: 'fake input',
-            context: {},
-            username: 'foo',
-            password: 'bar',
-            workspace_id: workspace_id
-        };
-        return action.main(params).then(function() {
-            assert(true);
-        }).catch(assert.ifError);
-    })
-})
+  it('should call Conversation when the parameters are right', function() {
+    const params = {
+      input: 'fake input',
+      context: {},
+      username: 'foo',
+      password: 'bar',
+      workspace_id: workspace_id
+    };
+    return action.main(params).then(function() {
+      assert(true);
+    }).catch(assert.ifError);
+  });
+});
--- tests/actions/discovery-test.js
+++ tests/actions/discovery-test.js
@@ -7,70 +7,70 @@ const collection_id = 'fake-id';
 
 describe('[action] Discovery', function() {
 
-    beforeEach(function() {
-        nock('https://gateway.watsonplatform.net:443')
-            .get(`/discovery/api/v1/environments/${environment_id}/collections/${collection_id}/query`)
-            .query({
-                'version': '2016-12-01'
-            })
-            .reply(200, {
-                results: [
-                    {
-                        contentHtml: "test content",
-                        score: "42",
-                        id: "24601",
-                        sourceUrl: "ibm.com",
-                        title: "Hello, world!"
-                    }
-                ]
-            });
-    });
+  beforeEach(function() {
+    nock('https://gateway.watsonplatform.net:443')
+      .get(`/discovery/api/v1/environments/${environment_id}/collections/${collection_id}/query`)
+      .query({
+        'version': '2016-12-01'
+      })
+      .reply(200, {
+        results: [
+          {
+            contentHtml: 'test content',
+            score: '42',
+            id: '24601',
+            sourceUrl: 'ibm.com',
+            title: 'Hello, world!'
+          }
+        ]
+      });
+  });
 
-    it('should throw error if credentials are missing', function() {
-        const params = {
-            input: 'fake input',
-            output: {},
-            environment_id: environment_id,
-            collection_id: collection_id
-        };
-        return action.main(params).then(function() {
-            assert.fail('Missing credentials error was not found');
-        }).catch(function(error) {
-            assert(error.message === 'params.username cannot be null');
-        });
+  it('should throw error if credentials are missing', function() {
+    const params = {
+      input: 'fake input',
+      output: {},
+      environment_id: environment_id,
+      collection_id: collection_id
+    };
+    return action.main(params).then(function() {
+      assert.fail('Missing credentials error was not found');
+    }).catch(function(error) {
+      assert(error.message === 'params.username cannot be null');
     });
+  });
 
-    it('should call Discovery when the parameters are right', function() {
-        const params = {
-            input: 'fake input',
-            output: {'action': {'call_discovery': ''}},
-            username: 'foo',
-            password: 'bar',
-            environment_id: environment_id,
-            collection_id: collection_id
-        };
-        return action.main(params).then(function(response) {
-            assert(response.output.hasOwnProperty("discoveryResults"));
-        }).catch(assert.ifError);
-    });
+  it('should call Discovery when the parameters are right', function() {
+    const params = {
+      input: 'fake input',
+      output: {'action': {'call_discovery': ''}},
+      username: 'foo',
+      password: 'bar',
+      environment_id: environment_id,
+      collection_id: collection_id
+    };
+    return action.main(params).then(function(response) {
+      assert(response.output.hasOwnProperty('discoveryResults'));
+    }).catch(assert.ifError);
+  });
 
-    it('should do nothing to the input if call_discovery is not present', function() {
-        const params = {
-            input: 'fake input',
-            context: {},
-            output: {'text': 'hello'},
-            username: 'foo',
-            password: 'bar',
-            environment_id: environment_id,
-            collection_id: collection_id
-        };
-        let expected = JSON.parse(JSON.stringify(params));
-        delete expected.username;
-        delete expected.password;
-        delete expected.environment_id;
-        delete expected.collection_id;
-        return action.main(params).then(function(output) {
-            assert(JSON.stringify(output) === JSON.stringify(expected));
-        }).catch(assert.ifError);
-    });
+  it('should do nothing to the input if call_discovery is not present', function() {
+    const params = {
+      input: 'fake input',
+      context: {},
+      output: {'text': 'hello'},
+      username: 'foo',
+      password: 'bar',
+      environment_id: environment_id,
+      collection_id: collection_id
+    };
+    let expected = JSON.parse(JSON.stringify(params));
+    delete expected.username;
+    delete expected.password;
+    delete expected.environment_id;
+    delete expected.collection_id;
+    return action.main(params).then(function(output) {
+      assert(JSON.stringify(output) === JSON.stringify(expected));
+    }).catch(assert.ifError);
+  });
 });
