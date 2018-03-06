48e14ff126c955d36a190dc625833028e1686630
NikoruChen <shiland@163.com>
2018-03-05 19:23:58 -0500

    speech to text

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
+import AudioRecorder from 'react-audio-recorder';
 import './App.css';
 import Conversation from './Conversation.js';
 import DiscoveryResult from './DiscoveryResult.js';
 const TEXT_TO_SPEECH_URL = 'https://openwhisk.ng.bluemix.net/api/v1/namespaces/nicole.nikoru.liu.chen%40gmail.com_dev/actions/Bluemix_text-to-speech-cs89_text-to-speech-cs89-chat-text-to-sp-1517884259798/textToSpeech?blocking=true&result=true';
+const SPEECH_TO_TEXT_URL = 'https://openwhisk.ng.bluemix.net/api/v1/namespaces/nicole.nikoru.liu.chen%40gmail.com_dev/actions/Bluemix_speech-to-text-cs89_speech-to-text-gsakl-speech-to--1517882863030/speechToText?blocking=true&result=true';
 
 class App extends Component {
   constructor(props) {
@@ -18,6 +20,83 @@ class App extends Component {
     this.handleSubmit = this.handleSubmit.bind(this);
     this.callWatson('hello');
     this.getSpeech = this.getSpeech.bind(this);
+    this.onAudioChange = this.onAudioChange.bind(this);
+    // this.transcribe = this.transcribe.bind(this);
+    this.getData = this.getData.bind(this);
+  }
+  //
+  // transcribe(audioFile) {
+  //   console.log(audioFile);
+  //   return fetch(SPEECH_TO_TEXT_URL, {
+  //     method: 'POST',
+  //     headers: {
+  //       'Content-Type': 'application/json',
+  //       'Accept': 'application/json',
+  //       'Authorization': 'Basic MDJmMTJmNDQtN2YwNy00NDlmLTgwOTYtZWMyYzU4ZjA1NTE5OjU3d0tvdUI2d0k3c0NrczFGR1JZSWxMb1pVUHJFR29rS3ZPZHdzNzREem4zWFBEbmhJRnNKWmZBanFDOFVWVUw='
+  //     },
+  //     body: JSON.stringify({
+  //       content_type: 'audio/wav',
+  //       encoding: 'base64',
+  //       payload: audioFile
+  //     })
+  //   }).then(response=>{
+  //     console.log(response);
+  //   })
+  //     .catch(err => {
+  //       console.log(err);
+  //     });
+  // }
+
+  getData(audioFile) {
+    // var reader = new FileReader();
+    // reader.onload = function(event) {
+    //   var data = event.target.result.split(',')
+    //     , decodedImageData = btoa(data[1]);                    // the actual conversion of data from binary to base64 format
+    //   callback(decodedImageData);
+    // };
+    // reader.readAsDataURL(audioFile);
+    // const transcribe = this.transcribe;
+    var reader = new FileReader();
+    reader.readAsDataURL(audioFile);
+    reader.onloadend = function() {
+      // console.log(reader.result);
+      const base64 = reader.result.substring(reader.result.indexOf(',')+1);
+      // console.log(base64);
+      // console.log(base64);
+      return fetch(SPEECH_TO_TEXT_URL, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          'Accept': 'application/json',
+          'Authorization': 'Basic MDJmMTJmNDQtN2YwNy00NDlmLTgwOTYtZWMyYzU4ZjA1NTE5OjU3d0tvdUI2d0k3c0NrczFGR1JZSWxMb1pVUHJFR29rS3ZPZHdzNzREem4zWFBEbmhJRnNKWmZBanFDOFVWVUw='
+        },
+        body: JSON.stringify({
+          content_type: 'audio/wav',
+          encoding: 'base64',
+          payload: base64
+        })
+      }).then(response=>{
+        console.log(response);
+        return response.json().then(responseJson => {
+          console.log(responseJson);
+        });
+      })
+        .catch(err => {
+          console.log(err);
+        });
+    };
+  }
+
+  onAudioChange(blob) {
+    console.log(blob);
+    // const transcribe = this.transcribe;
+    // var reader = new FileReader();
+    // reader.readAsDataURL(blob.audioData);
+    // reader.onloadend = function() {
+    //   console.log(reader.result);
+    //   transcribe(blob.audioData);
+    // };
+    this.getData(blob.audioData);
   }
 
   getSpeech(msgObj) {
@@ -31,7 +110,8 @@ class App extends Component {
       },
       body: JSON.stringify({
         payload: msgObj.message,
-        accept: 'audio/wav'
+        accept: 'audio/wav',
+        encoding: 'base64'
       })
 
     })
@@ -158,6 +238,7 @@ class App extends Component {
           onSubmit={this.handleSubmit}
           messageObjectList={this.state.messageObjectList}
         />
+        <AudioRecorder onChange={this.onAudioChange}/>
       </div>
     );
   }
